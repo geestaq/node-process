65d9c9fa320682873b754976de4c15f9a6b37ffa
Rafał Nowakowski <gee@vp.pl>
2019-05-02 12:42:20 +0200

    Add time converter module

---
+++ app/program.js
@@ -0,0 +1,31 @@
+//zaladowanie modulow
+const OSinfo = require('../modules/OSInfo');
+//console.log(this);
+//process.exit();
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    const input = process.stdin.read();
+	if(input !== null) {
+		const instruction = input.toString().trim();
+		switch(instruction) {
+			case '/nodeversion':
+				process.stdout.write('Wersja Node.js: ' + process.versions.node + '\n');
+				break;
+			case '/lang':
+				process.stdout.write('Język: ' + process.env.LANG + '\n');
+				break;
+			case '/getOSinfo':
+				OSinfo.print();
+				break;
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		}
+    }
+});
